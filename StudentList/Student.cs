b4d7663fd0d1420a39349e4c5e46417c1ef4f514
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-19 09:41:48 +0900

    초기 커밋.

---
+++ StudentList/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList
+{
+    //모델 클래스
+    class Student
+    {      
+        public string id;
+        public string name;
+        public int grade;
+        public string major;
+        public DateTime birthday;
+    }
+}
